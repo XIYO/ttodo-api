5a911c030729fd95bd532cec16f5acb8bb7d3db2
xiyo <bunny@xiyo.dev>
2024-12-31 16:07:23 +0900

    :wrench: 디비 세팅을 Mysql에 맞게 변경

--- zzic-back/src/main/resources/db/migration/V1__INIT.sql
+++ zzic-back/src/main/resources/db/migration/V1__INIT.sql
@@ -1,15 +1,7 @@
-ALTER USER sa SET PASSWORD 'root';
-
 -- 테이블 생성
 CREATE TABLE todo (
-                      id SERIAL PRIMARY KEY,
-                      title VARCHAR(255) NOT NULL,
-                      description TEXT,
-                      done BOOLEAN NOT NULL DEFAULT FALSE
+                      id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'To-Do 항목의 고유 식별자',
+                      title VARCHAR(255) NOT NULL COMMENT 'To-Do 항목의 제목',
+                      description TEXT COMMENT 'To-Do 항목의 상세 설명',
+                      done BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'To-Do 항목 완료 여부'
 );
-
--- 컬럼별 주석 추가
-COMMENT ON COLUMN todo.id IS 'To-Do 항목의 고유 식별자';
-COMMENT ON COLUMN todo.title IS 'To-Do 항목의 제목';
-COMMENT ON COLUMN todo.description IS 'To-Do 항목의 상세 설명';
-COMMENT ON COLUMN todo.done IS 'To-Do 항목 완료 여부';
