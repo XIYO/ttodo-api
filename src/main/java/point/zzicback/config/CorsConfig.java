60548259383534346b1cd24f2fb42797241cc83d
XIYO <bunny@xiyo.dev>
2025-05-16 12:06:56 +0900

    :art: CORS 설정을 위한 CorsConfig 클래스 추가

---
+++ src/main/java/point/zzicback/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package point.zzicback.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
