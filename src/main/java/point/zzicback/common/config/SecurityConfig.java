dd9f6c8ff8a8a7cc2eee179966abbc38e2a6b346
so460 <so728@naver.com>
2025-05-20 20:42:23 +0900

    yml https 설정

--- src/main/java/point/zzicback/common/config/SecurityConfig.java
+++ src/main/java/point/zzicback/common/config/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html"
                         ).permitAll()
-                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
