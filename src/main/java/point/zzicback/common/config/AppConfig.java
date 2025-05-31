23f0a7bb4a1bec5b5f743c16dee344f0e571e7b2
XIYO <bunny@xiyo.dev>
2025-05-31 15:06:53 +0900

    :coffin: not use code

---
+++ src/main/java/point/zzicback/common/config/AppConfig.java
@@ -0,0 +1,10 @@
+package point.zzicback.common.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import point.zzicback.auth.config.properties.JwtProperties;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class AppConfig {
+}
