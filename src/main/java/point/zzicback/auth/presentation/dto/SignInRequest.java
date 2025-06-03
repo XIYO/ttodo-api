9302016b8a2de3e8a9a956711faeaf33c64641cb
XIYO <bunny@xiyo.dev>
2025-06-03 13:05:17 +0900

    :recycle: PasswordOrAnonymousValid 및 PasswordOrAnonymousValidator 삭제, SignInRequest에서 해당 유효성 검사 제거

--- src/main/java/point/zzicback/auth/presentation/dto/SignInRequest.java
+++ src/main/java/point/zzicback/auth/presentation/dto/SignInRequest.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 @Schema(description = "사용자 사인-인 요청 DTO")
-@PasswordOrAnonymousValid
 public record SignInRequest(
         @Schema(description = "사용자 이메일", example = "user@example.com") @NotBlank(message = "이메일은 필수 입력 항목입니다.") @Email(message = "{email.valid}") String email,
         @Schema(description = "사용자 비밀번호", example = "Strong@123") String password) {
