1d3afb079230b92dc5339309e3079ac579712b7b
so460 <so728@naver.com>
2025-06-16 21:54:10 +0900

    익명사용자만 투두 자동생성

--- src/main/java/point/zzicback/todo/domain/TodoRepository.java
+++ src/main/java/point/zzicback/todo/domain/TodoRepository.java
@@ -7,16 +7,13 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.*;
 
-/**
- * Todo 도메인을 위한 Repository 인터페이스
- * DDD 원칙에 따라 도메인 계층에 위치
- */
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     Page<Todo> findByMemberIdAndStatus(UUID memberId, TodoStatus status, Pageable pageable);
     Page<Todo> findByMemberId(UUID memberId, Pageable pageable);
     Optional<Todo> findByIdAndMemberId(Long todoId, UUID memberId);
-    Optional<Todo> findByMemberIdAndTitle(UUID memberId, String title);
+    long countByMemberId(UUID memberId);
     
     @Query("SELECT t FROM Todo t WHERE t.member.id = :memberId AND t.status = 'IN_PROGRESS' AND t.dueDate < :currentDate")
     Page<Todo> findOverdueTodos(@Param("memberId") UUID memberId, @Param("currentDate") LocalDate currentDate, Pageable pageable);
 }
+
